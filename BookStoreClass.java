83bb46540fee169f448d2c5c9fa689dcbb1eacc1
Sans01010 <Santinolegovender@gmail.com>
2025-04-18 19:17:12 +0200

    Create BookStoreClass.java

---
+++ BookStoreClass.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Books;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Books {
+
+    private String Title;
+    private String Author;
+    private double Price;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
